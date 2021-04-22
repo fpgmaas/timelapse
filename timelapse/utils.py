b8a781c67ad49d514a8f8e3df6878f80fb7ef55f
maasf2 <florian.maas@vodafoneziggo.com>
2021-04-22 09:36:32 +0200

    properties instead of cap

--- timelapse/utils.py
+++ timelapse/utils.py
@@ -4,39 +4,40 @@ import cv2
 import logging
 import numpy as np
 
-def find_correct_exposure(cap,min_brightness=100,max_brightness=125, max_iter=1000, step = 5, verbose = False):
-    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+def find_correct_exposure(properties,min_brightness=100,max_brightness=125, max_iter=1000, step = 5):
     exposure_fixed = False
-    exposure = cap.get(15)
-    if verbose: 
-        logging.info(f'Starting find_correct exposure, current exposure: {exposure}')
+    exposure = properties['CV_CAP_PROP_EXPOSURE']
+    MAX_EXPOSURE = 2047
+    MIN_EXPOSURE = 3
+
+    logging.info(f'Starting find_correct exposure, current exposure: {exposure}')
     n_iter=0
     while not exposure_fixed:
+        cap = init_cap_dict(properties)
         ret,frame = cap.read()
         brightness = calculate_brightness(frame)
         if brightness <= min_brightness:
-            exposure = np.min([2047, exposure+step])
-            cap.set(cv2.CAP_PROP_EXPOSURE, exposure)
+            exposure = np.min([MAX_EXPOSURE, exposure+step])
+            properties['CV_CAP_PROP_EXPOSURE'] = exposure
             n_iter+=1
-            if verbose: 
-                logging.info(f'Iteration {n_iter-1} - Brightness: {brightness}. Increased exposure to {exposure}')
+            logging.info(f'Iteration {n_iter-1} - Brightness: {brightness}. Increased exposure to {exposure}')
         elif brightness >= max_brightness:
-            exposure = np.max([0, exposure-step])
-            cap.set(cv2.CAP_PROP_EXPOSURE, exposure)
+            exposure = np.max([MIN_EXPOSURE, exposure-step])
+            properties['CV_CAP_PROP_EXPOSURE'] = exposure
             n_iter+=1
-            if verbose: 
-                logging.info(f'Iteration {n_iter-1} - Brightness: {brightness}. Decreased exposure to {exposure}')
+            logging.info(f'Iteration {n_iter-1} - Brightness: {brightness}. Decreased exposure to {exposure}')
         elif n_iter>max_iter:
             logging.info(f'Iteration {n_iter-1} - Limit exceeded - Brightness: {brightness}. Exposure was {exposure}')
             exposure_fixed=True
         else:
             logging.info(f'Iteration {n_iter-1} - Acceptable exposure reached - Brightness: {brightness}. Exposure was {exposure}')
             exposure_fixed=True
-        if exposure == 0 or exposure == 2047:
+        if exposure == MIN_EXPOSURE or exposure == MAX_EXPOSURE:
             exposure_fixed=True
             logging.info(f'Iteration {n_iter-1} - Exposure limit reached - Brightness: {brightness}. Exposure was {exposure}')
+        end_cap(cap)
 
-    return cap, exposure, n_iter
+    return properties, n_iter
 
 def calculate_brightness(image_array):
     """
@@ -51,29 +52,52 @@ def calculate_sharpness(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return cv2.Laplacian(gray, cv2.CV_64F).var()
 
-def find_best_focus(cap):
+def find_best_focus(properties):
     logging.info('Finding the optimal focus.')
-    cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
     sharpness_dict = dict()
     for focus in range(0,230,5):
-        cap.set(cv2.CAP_PROP_FOCUS ,focus)
+        properties['CV_CAP_PROP_FOCUS'] = focus
+        cap = init_cap_dict(properties)
         ret,frame = cap.read()
         sharpness_dict[focus] = calculate_sharpness(frame)
+        end_cap(cap)
     
     logging.info('Sharpness per focus value: ' + ', '.join([f'{k}:{round(v,1)}' for k, v in sharpness_dict.items()]))
     best_focus = max(sharpness_dict, key=sharpness_dict.get)
     logging.info(f'Best focus at {best_focus}, {sharpness_dict[best_focus]}')
-    cap.set(cv2.CAP_PROP_FOCUS ,best_focus)
-    return cap, best_focus
+    properties['CV_CAP_PROP_FOCUS'] = best_focus
+    return properties
 
 def get_properties(cap):
     properties = {
-        10:'CV_CAP_PROP_BRIGHTNESS',
-        11:'CV_CAP_PROP_CONTRAST',
-        12:'CV_CAP_PROP_SATURATION',
-        13:'CV_CAP_PROP_HUE',
-        14:'CV_CAP_PROP_GAIN',
+        3: 'CAP_PROP_FRAME_WIDTH',
+        4: 'CAP_PROP_FRAME_HEIGHT',
         15:'CV_CAP_PROP_EXPOSURE',
         28:'CV_CAP_PROP_FOCUS'
+        39: 'CV_CAP_PROP_AUTOFOCUS'
     }
     return {value : cap.get(key) for key,value in properties.items()}
+
+def init_cap_dict(properties):
+    return init_cap(
+        width = properties['CAP_PROP_FRAME_WIDTH'],
+        height = properties['CAP_PROP_FRAME_HEIGHT'],
+        exposure = properties['CV_CAP_PROP_EXPOSURE'],
+        focus = properties['CV_CAP_PROP_FOCUS'],
+        autofocus = properties['CV_CAP_PROP_AUTOFOCUS']
+    )
+
+def init_cap(width=1280, height=720, autofocus=0, exposure=None, focus=None):      
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    if exposure is not None:
+        cap.set(cv2.CAP_PROP_EXPOSURE, exposure)
+    cap.set(cv2.CAP_PROP_AUTOFOCUS, autofocus)
+    if focus is not None:
+        cap.set(cv2.CAP_PROP_FOCUS, focus)
+    return cap
+
+def end_cap(cap):
+    cap.release()
+    cv2.destroyAllWindows()
